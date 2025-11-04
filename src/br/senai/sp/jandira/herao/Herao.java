3e327da9ba7633578216c7fef16750f2dd59abfc
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-04 20:09:25 -0300

    Funções iniciais

---
+++ src/br/senai/sp/jandira/herao/Herao.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.herao;
+
+import br.senai.sp.jandira.herao.model.Usuario;
+
+public class Herao {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Usuario usuario = new Usuario();
+
+        usuario.obterLadosDoTriangulo();
+    }
+}
---
+++ src/br/senai/sp/jandira/herao/model/Usuario.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.herao.model;
+
+public class Usuario {
+
+    int a;
+    int b;
+    int c;
+    int s;
+
+    public void obterLadosDoTriangulo(){
+
+    }
+}
