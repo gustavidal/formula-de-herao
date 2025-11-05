3c784408284b6254b89f98aa28651480e88d4578
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-04 21:40:16 -0300

    Função de identificação do tipo de triângulo

--- src/br/senai/sp/jandira/herao/model/Usuario.java
+++ src/br/senai/sp/jandira/herao/model/Usuario.java
@@ -9,6 +9,7 @@ public class Usuario {
     int c;
     double s;
     double area;
+    String triangulo;
 
     public void obterLadosDoTriangulo(){
         Scanner leitor = new Scanner(System.in);
@@ -36,16 +37,27 @@ public class Usuario {
         System.out.println("");
 
         if ((a + b) > c && (b + c) > a && (c + a) > b){
-            System.out.println("Os valores informados fazem um triângulo real.");
+            System.out.println("Os valores informados formam um triângulo real.");
+            identificarTipoDoTriangulo();
             realizarContas();
-        } else if (((a + b) < c || (b + c) < a || (c + a) < b)){
-            System.out.println("Os valores informados não fazem um triângulo real!");
-            System.out.println("Sem solução!");
+        } else {
+            System.out.println("Os valores informados NÃO formam um triângulo real!");
+            System.out.println("Sem solução (área indefinida)");
+        }
+    }
+
+    public void identificarTipoDoTriangulo(){
+        if (a == b && a == c){
+            triangulo = "equilátero, tendo os 3 lados iguais.";
+        } else if (a == b || b == c || c == a){
+            triangulo = "isósceles, tendo 2 lados iguais.";
+        } else {
+            triangulo = "escaleno, tendo seus 3 lados diferentes.";
         }
     }
 
     public void realizarContas(){
-        s = (a + b + c) / 2;
+        s = (a + b + c) / 2.0;
 
         double sA = s - a;
         double sB = s - b;
@@ -61,6 +73,7 @@ public class Usuario {
 
         System.out.println("");
         System.out.println("-------------------------------------");
+        System.out.println("Este triângulo é " + triangulo);
         System.out.println("Lado a: " + a);
         System.out.println("Lado b: " + b);
         System.out.println("Lado c: " + c);
